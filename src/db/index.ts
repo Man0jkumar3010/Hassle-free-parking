bc647a21e384f2405dd315b840a565e3d60ccdbc
Manojkumar <manojkumar.fsd3@gmail.com>
2025-08-21 14:44:06 +0530

    Deploy to vercel

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,17 +3,23 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from '@/db/schemas';
 
+// const pool = new Pool({
+//   host: process.env.POSTGRES_HOST!,
+//   port: parseInt(process.env.POSTGRES_PORT!),
+//   password: process.env.POSTGRES_PASSWORD!,
+//   user: process.env.POSTGRES_USER!,
+//   database: process.env.POSTGRES_DATABASE!,
+//   ssl:
+//     process.env.POSTGRES_SSL_ENABLED === "true"
+//       ? { rejectUnauthorized: false }
+//       : false,
+// });
+
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST!,
-  port: parseInt(process.env.POSTGRES_PORT!),
-  password: process.env.POSTGRES_PASSWORD!,
-  user: process.env.POSTGRES_USER!,
-  database: process.env.POSTGRES_DATABASE!,
-  ssl:
-    process.env.POSTGRES_SSL_ENABLED === "true"
-      ? { rejectUnauthorized: false }
-      : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required for Supabase
 });
+
 const db = drizzle({ client: pool, schema, logger: true });
 
 export default db;
